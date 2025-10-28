e3038833652b6dffbb51784c0295cb47dd953817
turnDeep <62247401+turnDeep@users.noreply.github.com>
2025-10-28 12:39:05 +0900

    Initialize __init__.py for the package

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+# src/__init__.py
+"""
+マススペクトル予測パッケージ
+"""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+
+from .models import *
+from .data import *
+from .training import *
+from .utils import *
